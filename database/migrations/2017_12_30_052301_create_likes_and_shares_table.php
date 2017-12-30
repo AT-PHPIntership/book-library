03c31bad6c127a4d720ea3c54b423dd4d38598e4
at-annguyen <ngqanlc1995@gmail.com>
2017-12-30 17:25:53 +0700

    Data Migraton

---
+++ database/migrations/2017_12_30_052301_create_likes_and_shares_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesAndSharesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes_and_shares', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->foreign('post_id')
+                    ->references('id')->on('posts')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                    ->references('id')->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->integer('like');
+            $table->integer('share');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes_and_shares');
+
+    }
+}
