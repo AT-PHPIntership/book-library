3946000f29061a719d12f40fbe0aea2eb6456c68
at-annguyen <ngqanlc1995@gmail.com>
2017-12-30 21:50:28 +0700

    Model and Relationship

---
+++ app/Model/Book.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    /**
+     * Declare table
+     *
+     * @var string $tabel table name
+     */
+    protected $table = 'books';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'category_id',
+        'name',
+        'author',
+        'year',
+        'description',
+        'donate_by',
+        'avg_rating',
+        'total_rating',
+        'image',
+        'status'
+    ];
+
+    /**
+     * Relationship hasMany with Post
+     *
+     * @return array
+    */
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    /**
+     * Relationship belongsToMany with User
+     *
+     * @return array
+    */
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * Relationship belongsTo with Category
+     *
+     * @return array
+    */
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
