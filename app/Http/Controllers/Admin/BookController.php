aeea30cb4ea41406aa798aa98fe73ba36b3669d3
at-anhngo <anh.ngo@asiantech.vn>
2018-01-02 13:12:12 +0700

    remove action index

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -8,16 +8,6 @@ use App\Http\Controllers\Controller;
 class BookController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
