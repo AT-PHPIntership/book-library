fa35752e87b6c9053b342c1e580efa2233890ae9
at-anhngo <anh.ngo@asiantech.vn>
2018-01-02 15:19:31 +0700

    get categories list

---
+++ app/Http/Controllers/Admin/BookController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\Category;
+
+class BookController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $field = [
+            'id',
+            'name'
+        ];
+        $category = Category::select($field)->get();
+        dd($category);
+    }
+}
