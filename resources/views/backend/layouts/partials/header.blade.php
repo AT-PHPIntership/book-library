4ac036803f38c53c452f09e293ef0644b6d8ed7f
Thanh Tam <tam.nguyen4@asiantech.vn>
2018-01-03 16:18:27 +0700

    delete some unnecessary package

--- resources/views/backend/layouts/partials/header.blade.php
+++ resources/views/backend/layouts/partials/header.blade.php
@@ -4,6 +4,9 @@
     <span class="logo-lg"><b>{{__('Admin ')}}</b>{{__('Management')}}</span>
   </a>
   <nav class="navbar navbar-static-top">
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+      <span class="sr-only">Toggle navigation</span>
+    </a>
     <div class="navbar-custom-menu">
       <ul class="nav navbar-nav">
         <li class="dropdown user user-menu">
