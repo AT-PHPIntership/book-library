3fa118ef0aaa9aeec990d58cc87e62f60f7d045e
at-anhngo <anh.ngo@asiantech.vn>
2018-01-03 10:03:18 +0700

    fix donator field request, adding custom message

--- app/Http/Requests/BookCreateRequest.php
+++ app/Http/Requests/BookCreateRequest.php
@@ -28,7 +28,7 @@ class BookCreateRequest extends FormRequest
             'author' => 'required',
             'category_id' => 'required|numeric',
             'price' => 'required|numeric',
-            'donate_by' => 'required|exists:user,employee_code',
+            'donator' => 'required|exists:user,employee_code',
             'year' => 'required|numeric',
             'description' => 'required|string',
             'image'=> 'image|max:10240',
