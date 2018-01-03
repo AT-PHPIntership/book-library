41261cab70a565d3a2c2467d1c16c23d4921c33f
at-anhngo <anh.ngo@asiantech.vn>
2018-01-03 18:24:12 +0700

    save data

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BookCreateRequest;
 use App\Model\Category;
+use App\Model\Book;
+use App\Model\User;
 
 class BookController extends Controller
 {
@@ -22,5 +24,41 @@ class BookController extends Controller
         ];
         $categories = Category::select($fields)->get();
         return view('backend.books.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BookCreateRequest $request)
+    {
+        $book = new Book($request->toArray());
+        // save image path, move image to directory
+        if(isset($request->image)) {
+            $image = $request->image;
+            $name = config('image.name_prefix') . "-" . $image->hashName();
+            $folder = config('image.books.path_upload');
+            $image->move($folder, $name);
+
+            $path = $folder . $name;
+            $book->image = $path;
+        } else {
+            $path = config('image.no_image');
+            $book->image = $path;
+        }
+        //save generate qrcode
+        $book->QRcode = $book->generateQRcode();
+        //save donator id
+        $book->donator_id = User::where('employee_code', $request->donator_id)->get()[0]['id'];
+        $result = $book->save();
+        if ($result) {
+            flash(__('Create success'))->success();
+            return redirect()->route('books.create');
+        } else {
+            flash(__('Create failure'))->error();
+            return redirect()->back()->withInput();
+        }
     }
 }
