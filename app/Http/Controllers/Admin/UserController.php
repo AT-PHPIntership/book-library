fb774101884511b48adcc3206d6477cc1ded8e8b
Nghia Vo V <nghia.vo@asiantech.vn>
2018-01-03 16:59:26 +0700

    Create_Resoure_Detail_User

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of User.
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display User Detail.
+     *
+     * @return mixed
+     */
+    public function show()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Admin 
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+   Route::resource('user', 'UserController', ['except' => ['create', 'store']]);
+});
