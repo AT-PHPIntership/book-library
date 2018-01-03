8e32f1674808d0e9121ce9d8ce9fe422eb0f91c0
at-phutran2 <phu.tran2@asiantech.vn>
2018-01-03 12:03:41 +0700

    update convention to fix ci

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-	public function index()
-	{
-		return view('backend.layouts.books.list');
-	}
+    /**
+    * Index
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function index()
+    {
+        return view('backend.layouts.books.list');
+    }
 }
