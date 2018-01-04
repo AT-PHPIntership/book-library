65513f87ca97e6f0f04a76a882a6458a6ae86b31
at-annguyen <an.nguyen@asiantech.vn>
2018-01-04 17:22:36 +0700

    Handle API Login

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,10 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/login', 'Admin\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Admin\LoginController@login');
-Route::post('/logout', 'Admin\LoginController@showLoginForm')->name('logout');
+Route::post('/logout', 'Admin\LoginController@logout')->name('logout');
 //Admin 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
    Route::resource('users', 'UserController', ['except' => ['create', 'store']]);
+   Route::resource('/', 'HomeController');   
 });
 
