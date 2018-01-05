b7ec87bd79a30a7b047fa659ca9fe594afb87b9e
at-phutran2 <phu.tran2@asiantech.vn>
2018-01-05 14:04:26 +0700

    delete pagination

--- resources/views/backend/layouts/books/list.blade.php
+++ resources/views/backend/layouts/books/list.blade.php
@@ -89,13 +89,6 @@
       </div>
     </section>
     <div class="box-footer clearfix">
-      <ul class="pagination pagination-sm no-margin pull-right">
-        <li><a href="#">&laquo;</a></li>
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">&raquo;</a></li>
-      </ul>
     </div>
     <!-- /.content -->
   </div>
