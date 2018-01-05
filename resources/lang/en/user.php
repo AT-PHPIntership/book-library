cdde37358418e20aaf6b5e726c18ae5a2311c6c4
Thanh Tam <tam.nguyen4@asiantech.vn>
2018-01-05 11:32:20 +0700

    create lang/en/user and fix trans text

---
+++ resources/lang/en/user.php
@@ -0,0 +1,11 @@
+<?php 
+return [
+  'list_users' => 'List Users',
+  'users_table' => 'Users List Table',
+  'employee_code' => 'Employee code',
+  'employee_name' => 'Name',
+  'employee_email' => 'Email',
+  'total_donated' => 'Total donated books',
+  'total_borrowed' => 'Total borrowed books',
+  'user_title' => 'Users'
+];
