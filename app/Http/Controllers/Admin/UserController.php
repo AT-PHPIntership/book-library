abe4f0c7edc2d08685916055b6381e7fe6de2c10
at-minhphan <minh.phan@asiantech.vn>
2018-01-05 23:57:26 +0700

    In fuction index: - Add users.team, users.role in variable $fields - Add variable $virtualsession - Add virtualsession in compact

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -21,15 +21,20 @@ class UserController extends Controller
             'users.employee_code',
             'users.name',
             'users.email',
+            'users.team',
+            'users.role',
             DB::raw('COUNT(DISTINCT(borrowings.id)) AS total_borrowed'),
             DB::raw('COUNT(DISTINCT(donators.id)) AS total_donated'),
         ];
+        // Virtual session
+        $virtualsession = User::select()->where('id', 4)->first();
+        
         $users = User::leftJoin('borrowings', 'borrowings.user_id', '=', 'users.id')
         ->leftJoin('donators', 'donators.user_id', '=', 'users.id')
         ->select($fields)
         ->groupBy('users.id')
         ->paginate(config('define.page_length'));
-        return view('backend.users.index', compact('users'));
+        return view('backend.users.index', compact('users', 'virtualsession'));
     }
 
     /**
