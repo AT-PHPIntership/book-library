3bd4980dc274a4f7aaa141a8da52139e2b1bea53
at-minhphan <minh.phan@asiantech.vn>
2018-01-06 18:10:08 +0700

    Add link on button Add Title flash

--- resources/views/backend/users/index.blade.php
+++ resources/views/backend/users/index.blade.php
@@ -21,61 +21,68 @@
     <div class="row">
       <div class="col-xs-12">
         <div class="box">
-          <div class="box-header">
-            <h3 class="box-title">{{ __('user.users_table') }}</h3>
-          </div>
-          <div class="box-body">
-            <table id="example2" class="table table-bordered table-hover">
-              <thead>
-                <tr>
-                  <th>No</th>
-                  <th>{{ __('user.employee_code') }}</th>
-                  <th>{{ __('user.employee_name') }}</th>
-                  <th>{{ __('user.employee_email') }}</th>
-                  <th>{{ __('user.total_donated') }}</th>
-                  <th>{{ __('user.total_borrowed') }}</th>
-                  @if (session()->get('team') == 'SA')
-                  <th>{{ __('user.role') }}</th>
-                  @endif
-                </tr>
-              </thead>
-              <tbody>
-
-                @foreach ($users as $user)
-                <tr>
-                  <td>{{ $user->id }}</td>
-                  <td>{{ $user->employee_code }}</td>
-                  <td>{{ $user->name }}</td>
-                  <td>{{ $user->email }}</td>
-                  <td>{{ $user->total_donated }}</td>
-                  <td>{{ $user->total_borrowed }}</td>
-                  @if (session()->get('team') == 'SA')
-                  <td>
-                    <a style="width: 70px"
-                    @if ($user->team == 'SA')
-                    disabled
-                    @endif
-
-                    class= 
-                    @if ($user->role)
-                    "btn btn-success" >Admin
-                    @else
-                    "btn btn-danger" >User
-                    @endif
-                  </a>
-                </td>
-                @endif
-
-                @endforeach
-              </tr>
-            </tbody>
-
-          </table>
-          {{ $users->links() }}
+          @php
+          if(session('notification')!=null){
+          echo "<div class=\"text-center btn-success my-4\"><h3>".session()->get('notification')."</h3></div>";
+        }
+        @endphp
+        <div class="box-header">
+          <h3 class="box-title">{{ __('user.users_table') }}</h3>
         </div>
+        <div class="box-body">
+          <table id="example2" class="table table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>{{ __('user.employee_code') }}</th>
+                <th>{{ __('user.employee_name') }}</th>
+                <th>{{ __('user.employee_email') }}</th>
+                <th>{{ __('user.total_donated') }}</th>
+                <th>{{ __('user.total_borrowed') }}</th>
+                @if (session()->get('team') == 'SA')
+                <th>{{ __('user.role') }}</th>
+                @endif
+              </tr>
+            </thead>
+            <tbody>
+
+              @foreach ($users as $user)
+              <tr>
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->employee_code }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->total_donated }}</td>
+                <td>{{ $user->total_borrowed }}</td>
+                @if (session()->get('team') == 'SA')
+                <td>
+                  <a style="width: 70px"
+                  @if ($user->team == 'SA')
+                  disabled
+                  @else
+                  href="/admin/users/changerole/{{$user->id}}"
+                  @endif
+
+                  class= 
+                  @if ($user->role)
+                  "btn btn-success" >Admin
+                  @else
+                  "btn btn-danger" >User
+                  @endif
+                </a>
+              </td>
+              @endif
+
+              @endforeach
+            </tr>
+          </tbody>
+
+        </table>
+        {{ $users->links() }}
       </div>
     </div>
-  </section>
+  </div>
+</section>
 </div>
 
 <!-- /.content -->
