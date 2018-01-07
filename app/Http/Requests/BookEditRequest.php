31a0fc43b69a5dee064e262b068d79a62bbc9152
at-anhngo <anh.ngo@asiantech.vn>
2018-01-07 16:20:00 +0700

    create book edit request

---
+++ app/Http/Requests/BookEditRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Carbon\Carbon;
+
+class BookEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:8',
+            'author' => 'required',
+            'category_id' => 'required|numeric',
+            'price' => 'required|numeric|exists:books,price',
+            'donator_id' => 'required|exists:users,employee_code',
+            'year' => 'required|digits:4|integer|min:1900|max:'.(Carbon::now()->year+1),
+            'description' => 'required|string',
+            'image'=> 'image|max:10240',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'donator_id.exists' => 'User does not exists',
+        ];
+    }
+}
