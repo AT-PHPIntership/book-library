5de8fc53be2c4fe9fd82a00ee20f9fa8e8181f80
Anh Ngo Q <anh.ngo@asiantech.vn>
2018-01-16 11:06:58 +0700

    fix config path for import image

--- config/image.php
+++ config/image.php
@@ -3,9 +3,6 @@
 return [
     'max_upload_size'  => '10240',
     'name_prefix'      => date('Y-m-d'),
-    'default_thumbnail'=> '/images/default/default-thumbnail.jpg',
-    'no_image'         => '/images/default/no-image.png',
-    'no_user_image'    => 'images/default/profile.png',
     'books'            => [
         'path_upload'  => 'images/books/',
         'no_image_name'=> 'no-image',
