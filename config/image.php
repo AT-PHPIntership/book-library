56fb67bf7366e81af5c713d87033f9a9a12e01d8
Anh Ngo Q <anh.ngo@asiantech.vn>
2018-01-16 15:11:33 +0700

    update book edit

---
+++ config/image.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'max_upload_size'  => '10240',
+    'name_prefix'      => date('Y-m-d'),
+    'default_thumbnail'=> '/images/default/default-thumbnail.jpg',
+    'no_image'         => '/images/default/no-image.png',
+    'no_user_image'    => '/images/default/profile.png',
+    'books'            => [
+        'path_upload' => 'images/books/',
+    ]
+];
