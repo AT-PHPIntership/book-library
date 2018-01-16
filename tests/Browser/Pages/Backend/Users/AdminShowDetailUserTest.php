53e3a5da09cef97af5247248c926832be1740b66
Nghia Vo V <nghia.vo@asiantech.vn>
2018-01-16 16:30:43 +0700

    test detail of user

--- tests/Browser/Pages/Backend/Users/AdminShowDetailUserTest.php
+++ tests/Browser/Pages/Backend/Users/AdminShowDetailUserTest.php
@@ -5,25 +5,46 @@ namespace Tests\Browser\Pages\Backend\Users;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use App\Model\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class AdminShowDetailUserTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+    /**
+    * A User with role 1 test example.
+    *
+    * @return void
+    */
+    public function userLogin()
+    {
+        factory(User::class, 1)->create([
+           'employee_code' => 'ATI0308',
+           'name'          => 'Nghia Vo V.',
+           'email'         => 'nghia.vo@asiantech.vn',
+           'team'          => 'PHP',
+           'role'          => 1,
+        ]);
+    }
 
     /**
      * Test route view admin show detail user.
      *
      * @return void
      */
+
     public function testRouteShowDetailUser()
     {
-        $user = User::find(1);
-        $this->browse(function (Browser $browser) use ($user) {
-            $browser->visit('/admin/users')
+        $this->userLogin();
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                ->visit('/admin/users')
+                ->assertSee('List Users')
                 ->click('#username')
-                ->assertPathIs('/admin/users/' . $user->employee_code);
+                ->visit('admin/users/ATI0308')
+                ->assertSee('Profile User');
         });
     }
-
+    
     /**
      * Test layout of detail User Page.
      *
@@ -31,11 +52,20 @@ class AdminShowDetailUserTest extends DuskTestCase
      */
     public function testLayoutDetailUser()
     {
+        $this->userLogin();
         $user = User::find(1);
         $this->browse(function (Browser $browser) use ($user) {
-            $browser->visit('/admin/users/' . $user->employee_code)
+            $browser->loginAs(User::find(1))
+                ->visit('/admin/users/' . $user->employee_code)
                 ->assertSee('Profile User')
-                ->assertSee('About');
+                ->assertSee('Follow')
+                ->assertSee('Borrowed')
+                ->assertSee('Donated')
+                ->assertSee('Borrowing')
+                ->assertSee('About')
+                ->assertSee('fullname')
+                ->assertSee('join_dated')
+                ->assertSee('email');
         });
     }
 
@@ -46,9 +76,11 @@ class AdminShowDetailUserTest extends DuskTestCase
      */
     public function testShowDetailUser()
     {
+        $this->userLogin();
         $user = User::find(1);
         $this->browse(function (Browser $browser) use ($user) {
-            $browser->visit('/admin/users/' . $user->employee_code)
+            $browser->loginAs(User::find(1))
+                ->visit('/admin/users/' . $user->employee_code)
                 ->assertSee($user->name)
                 ->assertSee($user->email);
         });
