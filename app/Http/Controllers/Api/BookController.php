5064e886f2a5793a90531d17d0f7282dcc978d7a
at-minhphan <minh.phan@asiantech.vn>
2018-01-17 01:52:14 +0700

    fix cirleCI

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -6,23 +6,23 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Model\Book;
 use DB;
-use Illuminate\Database\Eloquent\SoftDeletes
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BookController extends Controller
 {
-	/**
-	* Delete book and its relationship.
-	*
-	* @param int $id id of book
-	*
-	* @return \Illuminate\Http\Response
-	*/
-	public function destroy($id)
-	{
-		$book = Book::findOrFail($id);
-		$book->softDeletes();
-		$data = [
-		];
-		return response()->json($data);
-	}
+    /**
+    * Delete book and its relationship.
+    *
+    * @param int $id id of book
+    *
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->softDeletes();
+        $data = [
+        ];
+        return response()->json($data);
+    }
 }
