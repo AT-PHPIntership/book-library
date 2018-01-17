0bc540fcb9e8c557edb864f628fa00349f582a28
at-annguyen <ngqanlc1995@gmail.com>
2018-01-17 17:32:49 +0700

    Delete Books by ajax

--- public/app/js/delete_book.js
+++ public/app/js/delete_book.js
@@ -8,18 +8,23 @@ for (let $eachBtn of $btnDelete) {
 		$btnOK.setAttribute('data-id', $id.slice(7));
 	});
 }
-
+// var $url = "{{ url('') }}";
 //Add event for button OK in modal
 $btnOK.addEventListener('click', function () {
 	$id = $btnOK.getAttribute('data-id');
-	$token = $('meta[name="csrf_token"]').attr('content');
+	// $token = $('meta[name="csrf_token"]').attr('content');
 	$.ajax({
-		type: 'DELETE',
-		headers: { 'X-CSRF-TOKEN': $token },
-		url: '/admin/delete/' + $id + '/books',
-		data:{
+		type: 'POST',
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 		},
-		success: function (data) {	
+		url: $baseURL + '/admin/books/' + $id,
+		data:{
+			_method: 'delete',
+		},
+		dataType: 'JSON',
+		success: function ($data) {
+			console.log($data);
 			$btnDelete = document.getElementById('delete-'+$id);
 			$tr = $btnDelete.parentElement.parentElement;
 			$tr.setAttribute('style', 'background-color: #aaa');
