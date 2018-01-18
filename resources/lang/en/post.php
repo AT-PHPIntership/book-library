431bb41debccb1802500e0b66723c03978096794
tam.nguyen4 <tam.nguyen4@asiantech.vn>
2018-01-18 11:19:40 +0700

    layout list posts

---
+++ resources/lang/en/post.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'post_title' => 'Post',
+    'list_post' => 'List Posts',
+    'admin' => 'Admin',
+    'id' => 'ID',
+    'user_name' => 'User Name',
+    'type_post' => 'Type',
+    'short_content' => 'Short Content',
+    'post_date' => 'Post Date',
+    'total_comment' => 'Toltal Comment'
+];
