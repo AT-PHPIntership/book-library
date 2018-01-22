c01b14abefe74f3ee88a0e4a68a274ce8c69f4c2
at-luanle <luan.le@asiantech.vn>
2018-01-22 11:17:55 +0700

    fix function search book by name, author, all

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'search' => [
-        \App\Model\Book::BOOK_ALL => 'Name - Author',
-        \App\Model\Book::BOOK_NAME => 'Name',
-        \App\Model\Book::BOOK_AUTHOR => 'Author',
+        'all' => 'Name - Author',
+        'name' => 'Name',
+        'author' => 'Author',
     ],
     'comeback' => 'ComeBack',
     'enter_name' => 'Enter Name Or Author...',
