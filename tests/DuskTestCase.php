7f9da7bee0b978636233d24fe254644c4951a2b7
Anh Ngo Q <anh.ngo@asiantech.vn>
2018-01-23 04:02:29 +0700

    create test

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -6,6 +6,7 @@ use Laravel\Dusk\TestCase as BaseTestCase;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use App\Model\User;
 
 abstract class DuskTestCase extends BaseTestCase
 {
@@ -39,5 +40,21 @@ abstract class DuskTestCase extends BaseTestCase
                 ChromeOptions::CAPABILITY, $options
             )
         );
+    }
+
+    /**
+     * Adding user for testing
+     * 
+     * @return void
+     */
+    public function fakeUser() {
+        $user = [
+            'employee_code' => 'AT0286',
+            'name'          => 'SA Dinh Thi.',
+            'email'         => 'sa.as@asiantech.vn',
+            'team'          => 'SA',
+            'role'          => 1,
+        ];
+        $user = factory(User::class, 1)->create($user);
     }
 }
