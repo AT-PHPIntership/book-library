64752141c96fd7e71626368fe076df23509fc144
Nghia Vo V <nghia.vo@asiantech.vn>
2018-01-22 18:07:45 +0700

    Build layout of list categories

---
+++ resources/lang/en/category.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'title' => 'List Categories',
+    'admin' => 'Admin',
+    'category' => 'Category',
+    'id' => 'ID',
+    'name' => 'Name',
+    'number_of_books' => 'Number of Books',
+    
+];
