35fa77de94e8501f0b4012405efef6cd98473472
at-annguyen <an.nguyen@asiantech.vn>
2018-01-23 14:38:19 +0700

    Fix getCount function

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -4,39 +4,25 @@ use App\Model\User;
 use App\Model\Book;
 use App\Model\Post;
 use App\Model\Category;
+use Illuminate\Database\Eloquent\Model;
 
 if (!function_exists('getCount')) {
   
   /**
-   * Get percent progress name of database
-   *
-   * @param string $name name of database
-   *
-   * @return float
-   */
-    function getCount($name)
-    {
-        /**
-         * Response count
-         *
-         * @param int $count count table
-         */
-        $count = 0;
-        switch ($name) {
-            case User::class:
-                $count = User::count();
-                break;
-            case Book::class:
-                $count = Book::count();
-                break;
-            case Post::class:
-                $count = Post::count();
-                break;
-            default:
-                $count = Category::count();
-        }
-        return $count;
-    }
+  * Get percent progress name of database
+  *
+  * @param string $class class name
+  *
+  * @return int
+  */
+  function getCount($class)
+  {
+      $count = 0;
+      if (class_exists($class) && app($class) instanceof Model) {
+          $count = app($class)->count();
+      }
+      return $count;
+  }
 }
 
 if (!function_exists('activeRoute')) {
