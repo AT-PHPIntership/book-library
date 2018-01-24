8851f03f275ec2bed68ad5c30093b2ab184bd3c9
at-luanle <luan.le@asiantech.vn>
2018-01-24 13:43:29 +0700

    show detail post and update seeder

--- app/Model/Book.php
+++ app/Model/Book.php
@@ -101,7 +101,7 @@ class Book extends Model
     */
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::classb);
     }
 
     /**
--- resources/views/backend/posts/show.blade.php
+++ resources/views/backend/posts/show.blade.php
@@ -11,11 +11,16 @@
     </section>
     <section class="content">
       <div class="row">
+        @foreach ($posts as $post)
         <div class="col-md-3">
           <!-- Profile Image -->
           <div class="box box-primary">
             <div class="box-body box-profile">
-              <img class="img-thumbnail" src="{{ asset('images/books/no-image.png')}}" alt="User profile picture">
+                @if ($post->image == '')
+                  <img class="img-thumbnail" src="{{ asset('images/books/lrg.jpg')}}" alt="User profile picture">
+                @else
+                 <img class="img-thumbnail" src="{{ asset('images/books/'.$post->image)}}" alt="User profile picture">
+                @endif
             </div>
             <!-- /.box-body -->
           </div>
@@ -25,127 +30,134 @@
         <div class="col-md-9">
           <div class="nav-tabs-custom">
             <ul class="nav nav-tabs">
-              <li class="active"><a href="#activity" data-toggle="tab">{{ __('post.review') }}</a></li>
-              <li><a href="#timeline" data-toggle="tab">{{ __('post.status') }}</a></li>
-              <li><a href="#settings" data-toggle="tab">{{ __('post.find_book') }}</a></li>
+                @switch($post->type)
+                  @case(App\Model\Post::REVIEW_TYPE)
+                    <li class="active"><a href="#activity" data-toggle="tab">{{ strtoupper(__('post.review')) }}</a></li>
+                    @break
+                  @case(App\Model\Post::STATUS_TYPE)
+                    <li><a href="#timeline" data-toggle="tab">{{ strtoupper(__('post.status')) }}</a></li>
+                    @break
+                  @case(App\Model\Post::FIND_TYPE)
+                    <li><a href="#settings" data-toggle="tab">{{ strtoupper(__('post.find_book')) }}</a></li>
+                    @break
+                @endswitch
             </ul>
+            <!-- /.tab-content -->
+
             <div class="tab-content">
-              <div class="active tab-pane" id="activity">
-                <!-- Post -->
-                <div class="post">
-                  <div>
-                        <span class="username">
-                         <a href="#" class="h2">Jonathan Burke Jr.</a></br>
-                        </span>
-                        <ol class="breadcrumb">
-                          <li><b>{{ __('post.by') }} </b><i>User </i></li>
-                          <li><b>{{ __('post.score') }} :</b><i>9</i></li>
-                          <li><b>{{ __('post.date') }} :</b><i> 20.10.2017</i></li>
-                          <li><i class="fa fa-trash-o text-danger"></i></li>
-                        </ol>
-                  </div>
-                  <!-- /.user-block -->
-                  <p>
-                    Lorem ipsum represents a long-held tradition for designers,
-                    typographers and the like. Some people hate it and argue for
-                    its demise, but others ignore the hate as they create awesome
-                    tools to help create filler text for everyone from bacon lovers
-                    to Charlie Sheen fans.
-                  </p>
-                </div>
-                <!-- /.post -->
-              </div>
-              <!-- /.tab-pane -->
-              <div class="tab-pane" id="timeline">
-                  <!-- Post -->
-                  <div class="post">
-                    <div>
-                          <span class="username">
-                           <a href="#">Jonathan Burke Jr.</a></br>
-                          </span>
-                          <ol class="breadcrumb">
-                            <li><b>{{ __('post.by') }} </b><i>User </i></li>
-                            <li><b>{{ __('post.score') }} :</b><i>9</i></li>
-                            <li><b>{{ __('post.date') }} :</b><i> 20.10.2017</i></li>
-                            <li><i class="fa fa-trash-o text-danger"></i></li>
-                          </ol>
+                <!-- /.display review -->
+                @if ($post->type == 1)
+                    <div class="active tab-pane" id="activity">
+                        <!-- Post -->
+                        <div class="post">
+                            <div>
+                                <div class="h2">
+                                    {{$post->name}}
+                                </div>
+                                <ol class="breadcrumb">
+                                    <li><b>{{ __('post.score') }} :</b>
+                                        <i>
+                                        {{$post->rating}}
+                                        </i>
+                                    </li>
+                                    <li><b>{{ __('post.date') }} :</b><i> {{ $post->created_at }}</i></li>
+                                    <li><i class="fa fa-trash-o text-danger"></i></li>
+                                </ol>
+                            </div>
+                          <!-- /.user-block -->
+                            <p>
+                                {{ $post->content }}
+                            </p>
+                        </div>
+                        <!-- /.post -->
                     </div>
-                    <!-- /.user-block -->
-                    <p>
-                      Lorem ipsum represents a long-held tradition for designers,
-                      typographers and the like. Some people hate it and argue for
-                      its demise, but others ignore the hate as they create awesome
-                      tools to help create filler text for everyone from bacon lovers
-                      to Charlie Sheen fans.
-                    </p>
-                  </div>
-                  <!-- /.post -->
-              </div>
-              <!-- /.tab-pane -->
-              <div class="tab-pane" id="settings">
-                  <!-- Post -->
-                  <div class="post">
-                    <div>
-                          <span class="username">
-                           <a href="#">Jonathan Burke Jr.</a></br>
-                          </span>
-                          <ol class="breadcrumb">
-                            <li><b>{{ __('post.by') }} </b><i>User </i></li>
-                            <li><b>{{ __('post.score') }} :</b><i>9</i></li>
-                            <li><b>{{ __('post.date') }} :</b><i> 20.10.2017</i></li>
-                            <li><i class="fa fa-trash-o text-danger"></i></li>
-                          </ol>
+                @endif
+              <!-- /.end reivew -->
+
+              <!-- /.display status -->
+                @if ($post->type == 2)
+                    <div class="active tab-pane" id="activity">
+                        <!-- Post -->
+                        <div class="post">
+                            <div>
+                                <ol class="breadcrumb">
+                                    <li><b>{{ __('post.date') }} :</b><i> {{ $post->created_at }}</i></li>
+                                    <li><i class="fa fa-trash-o text-danger"></i></li>
+                                </ol>
+                            </div>
+                          <!-- /.user-block -->
+                            <p>
+                                {{ $post->content }}
+                            </p>
+                        </div>
+                        <!-- /.post -->
                     </div>
-                    <!-- /.user-block -->
-                    <p>
-                      Lorem ipsum represents a long-held tradition for designers,
-                      typographers and the like. Some people hate it and argue for
-                      its demise, but others ignore the hate as they create awesome
-                      tools to help create filler text for everyone from bacon lovers
-                      to Charlie Sheen fans.
-                    </p>
-                  </div>
-                  <!-- /.post -->
-              </div>
-              <!-- /.tab-pane -->
+                @endif
+              <!-- /.end status -->
+
+              <!-- /.display find book-->
+                @if ($post->type == 3)
+                    <div class="active tab-pane" id="activity">
+                        <!-- Post -->
+                        <div class="post">
+                            <div>
+                                <ol class="breadcrumb">
+                                    <li><b>{{ __('post.date') }} :</b><i> {{ $post->created_at }}</i></li>
+                                    <li><i class="fa fa-trash-o text-danger"></i></li>
+                                </ol>
+                            </div>
+                          <!-- /.user-block -->
+                            <p>
+                                {{ $post->content }}
+                            </p>
+                        </div>
+                        <!-- /.post -->
+                    </div>
+                @endif
+              <!-- /.end find book -->
             </div>
+
             <!-- /.tab-content -->
           </div>
           <!-- /.nav-tabs-custom -->
         </div>
         <!-- /.col -->
         <!-- /.col -->
+        @endforeach
+        @if ($comments->count() > 0)
         <div class="col-md-12">
-          <div class="nav-tabs-custom">
-            <ul class="nav nav-tabs">
-              <li class="active"><a href="#activity" data-toggle="tab">Comments</a></li>
-            </ul>
-            <div class="tab-content">
-              <div class="active tab-pane" id="activity">
-                <!-- Post -->
-                <div class="post">
-                    <table class="table">
-                     <tbody>
-                       <tr>
-                         <th scope="row">1</th>
-                         <td>Mark</td>
-                         <td><a href="#" class="pull-right btn-box-tool"><i class="btn btn-danger fa fa-trash-o text-danger"></i></i></a></td>
-                       </tr>
-                       <tr>
-                         <th scope="row">2</th>
-                         <td>Jacob</td>
-                         <td><a href="#" class="pull-right btn-box-tool"><i class="btn btn-danger fa fa-trash-o text-danger"></i></a></td>
-                       </tr>
-                     </tbody>
-                    </table>
+            <div class="nav-tabs-custom">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#activity" data-toggle="tab">Comments</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="active tab-pane" id="activity">
+                    <!-- Post -->
+
+                    <div class="post">
+                    @foreach ($comments as $key=>$comment)
+                        <div class="list-group">
+                            @if ($comment->parent_id == '')
+                            <div href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+                                <p class="mb-1">{{ $comment->content }} <a href="#" class="glyphicon glyphicon-remove text-warning pull-right"></a></p>
+                            @endif
+                            @if ($comment->children->count())
+                                @foreach ($comment->children as $index=>$chil)
+                                <small class="text-muted">{{$chil->content}} <a href="#" class="glyphicon glyphicon-remove text-warning pull-right"></a></small>
+                                @endforeach
+                            </div>
+                            @endif
+                        </div>
+                    @endforeach
+                    </div>
+                    <!-- /.post -->
+                  </div>
                 </div>
-                <!-- /.post -->
-              </div>
-            </div>
             <!-- /.tab-content -->
           </div>
-          <!-- /.nav-tabs-custom -->
+            <!-- /.nav-tabs-custom -->
         </div>
+        @endif
         <!-- /.col -->
       </div>
       <!-- /.row -->
