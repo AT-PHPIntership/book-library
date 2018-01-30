3f4790446c6653091d6f7f6bfbbe21a0895b411a
at-annguyen <ngqanlc1995@gmail.com>
2018-01-30 11:16:14 +0700

    Fix send mail success

--- app/Http/Controllers/Admin/SendMailController.php
+++ app/Http/Controllers/Admin/SendMailController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Model\User;
+use Carbon\Carbon;
+use App\Model\Borrowing;
+use App\Mail\BorrowedBookMail;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
 
 class SendMailController extends Controller
 {
     /**
      * Send mail to borrower.
      *
+     *@param App\Model\Borrowing $borrowing borrowing
+     *
      * @return \Illuminate\Http\Response
      */
-    public function sendMail()
+    public function sendMail(Borrowing $borrowing)
     {
-        return view('backend.email.index');
+        if (!canSendMail($borrowing->date_send_email)) {
+            flash(__('borrow.sent_mail'))->warning();
+            return redirect()->back()->withInput();
+        }
+        Mail::to($borrowing->users->email)->send(new BorrowedBookMail($borrowing));
+        $borrowing->date_send_email = Carbon::now();
+        $result = $borrowing->save();
+        if ($result && empty(Mail::failures())) {
+            flash(__('borrow.send_mail_success'))->success();
+        } else {
+            flash(__('borrow.send_mail_failure'))->error();
+        }
+        return redirect()->back()->withInput();
     }
 }
