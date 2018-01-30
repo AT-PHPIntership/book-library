a9d439793ef674624f817fee77741cb629c29bf8
Nghia Vo V <nghia.vo@asiantech.vn>
2018-01-30 18:12:11 +0700

    edit name of category by object

--- public/app/js/category.js
+++ public/app/js/category.js
@@ -1,27 +1,54 @@
-$(document).ready(function() {
-  $(document).on('click', '.btn-show-edit-modal', function() {
-    $('#idCategory').val($(this).attr('data-id'));
-    $('#nameCategory').val($(this).attr('data-name'));
-    $('#myModal').modal('show');
-  });
-  $('.modal-footer').on('click', '.editCategory', function() { 
-    token = $('meta[name="csrf_token"]').attr('content');
-    id = $("#idCategory").val();
-    name = $('#nameCategory').val();
-    $.ajax({
-      type: 'PUT',
-      headers: { 'X-CSRF-TOKEN': token },
-      url: '/admin/categories/' + id,
-      data: {
-        'name': name
-      },
-      success: function(request) {
-        $("#nameCategory" + id).html(request.name);
-        $('#edit-modal' + id).attr('data-name', request.name);
-      },
-      error: function() {
-        alert("Error, please try again.");
-      }
-    });
-  });
-});
+// Category Object 
+var category = (function(){
+  // Constructor
+  var category = function(){
+  }
+
+  // edit name of category method
+  var editNameCategory = function() {
+    classBtnEditNameCategories = document.getElementsByClassName('btn-show-edit-modal');
+    for (let classBtnEditNameCategory of classBtnEditNameCategories) {
+        classBtnEditNameCategory.addEventListener('click', function () {
+            $('#idCategory').val($(this).attr('data-id'));
+            $('#nameCategory').val($(this).attr('data-name'));
+            $('#myModal').modal('show');
+        });
+    }
+  }
+  
+  // update name of category method to database
+  var updateNameCategory = function() {
+      $('.btn-UpdateNameCategory').on('click', function () {
+        token = $('meta[name="csrf_token"]').attr('content');
+        id = $("#idCategory").val();
+        name = $('#nameCategory').val();
+        $.ajax({
+          type: 'PUT',
+          headers: { 'X-CSRF-TOKEN': token },
+          url: '/admin/categories/' + id,
+          data: {
+            'name': name
+          },
+          success: function(request) {
+            $("#nameCategory" + id).html(request.name);
+            $('#edit-modal' + id).attr('data-name', request.name);
+          },
+          error: function() {
+            alert("Error, please try again.");
+          }
+        });
+      });
+  }
+
+  // Prototype
+  category.prototype = {
+    constructor: category,
+    editNameCategory: editNameCategory,
+    updateNameCategory: updateNameCategory
+  }
+
+  // Return category;
+  return category;
+})();
+
+var newCategory = new category();
