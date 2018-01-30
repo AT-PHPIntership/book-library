20e2f0b68817cfac7b5cd392cc71dfb6b89ccd25
at-annguyen <ngqanlc1995@gmail.com>
2018-01-30 09:34:38 +0700

    Loading

--- public/app/js/borrow.js
+++ public/app/js/borrow.js
@@ -1,6 +1,8 @@
 $('.btn-check').click(function () {
   $id = $(this).attr('id');
+  $('#loading').hide();
 })
 $('.btn-confirm').click(function () {
   $('#form-confirm-'+$id).submit();
+  $('#loading').show()
 })
