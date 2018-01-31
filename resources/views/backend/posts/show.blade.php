2d80a37a873f863419d04cfe809b0213305bcb26
at-luanle <luan.le@asiantech.vn>
2018-01-31 14:30:36 +0700

    unit test for show detail post

--- resources/views/backend/posts/show.blade.php
+++ resources/views/backend/posts/show.blade.php
@@ -15,7 +15,7 @@
               <!-- Profile Image -->
               <div class="box box-primary">
                 <div class="box-body box-profile">
-                      <img class="img-thumbnail" src="{{ asset('images/books/'.$post->image_url) }}" alt="User profile picture">
+                      <img class="img-thumbnail post-image" src="{{ $post->image_url }}" alt="User profile picture">
                 </div>
                 <!-- End Profile Image -->
               </div>
@@ -30,24 +30,23 @@
                         <!-- Post -->
                         <div class="post">
                             <div>
-                                <div class="h2">
+                                <div class="h2 post-username">
                                     {{$post->users->name}}
                                 </div>
-                                <ol class="breadcrumb">
+                                <ol class="breadcrumb post-ratings">
                                     @if ($post->type == App\Model\Post::REVIEW_TYPE)
                                         <li><b>{{ __('post.score') }} :</b>
                                             <i> {{$post->rating}} </i>
                                         </li>
                                     @endif
-                                    <li><b>{{ __('post.date') }} :</b><i> {{ $post->created_at }}</i></li>
+                                    <li><b>{{ __('post.date') }} :</b><i class="post-date"> {{ date('d-m-Y', strtotime($post->created_at))}}</i></li>
                                     <li><i class="fa fa-trash-o text-danger"></i></li>
                                 </ol>
                             </div>
-                            <p>
+                            <p class="post-content">
                                 {{ $post->content }}
                             </p>
                         </div>
-                        <div class="bs-example">
                     </div>
                 </div>
               </div>
