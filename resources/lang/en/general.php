207aad01c3034e29ef2cb3f8e012078b994d06f7
Nghia Vo V <nghia.vo@asiantech.vn>
2018-02-01 14:48:11 +0700

    search and filter borrow layout

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -9,4 +9,10 @@ return [
     'enter_name' => 'Enter Name Or Author...',
     'enter_author' => 'Enter Author ...',
     'options' => 'Options',
+    'find_borrow' => 'Find users or books',
+    'filter_borrow' => [
+        'all' => 'Users & Books',
+        'users' => 'Users',
+        'books' => 'Books',
+    ],
 ];
