518f5f74d370693472d71493323171b3841a2f98
at-minhphan <minh.phan@asiantech.vn>
2018-02-03 02:47:38 +0700

    add file test delete book

---
+++ tests/Browser/Pages/Backend/Books/BaseTestBook.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Browser\Pages\Backend\Books;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use DB;
+use App\Model\Book;
+use App\Model\User;
+use App\Model\Category;
+use App\Model\Donator;
+use Faker\Factory as Faker;
+
+class BaseTestBook extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Make list of book.
+     *
+     * @param int $rows number book.
+     *
+     * @return Book.
+     */
+    public function makeListOfBook($rows)
+    {
+        $faker = Faker::create();
+        factory(Category::class, 10)->create();
+        factory(User::class, 10)->create();
+        $userIds = DB::table('users')->pluck('id')->toArray();
+        factory(Donator::class, 10)->create([
+            'user_id' => $faker->unique()->randomElement($userIds)
+        ]);
+        $categoryIds = DB::table('categories')->pluck('id')->toArray();
+        $donatorIds = DB::table('donators')->pluck('id')->toArray();
+        factory(Book::class, $rows)->create([
+            'category_id' => $faker->randomElement($categoryIds),
+            'donator_id' => $faker->randomElement($donatorIds),
+        ]);
+    }
+}
