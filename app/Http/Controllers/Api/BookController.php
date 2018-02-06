a73bc3fe13c8e6e8c0d7e212c990dad74b5e1ddc
tam.nguyen4 <tam.nguyen4@asiantech.vn>
2018-02-06 17:09:00 +0700

    list new books

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\Book;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+
+    /**
+     * The Book implementation.
+     *
+     * @var Book
+     */
+    protected $book;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param Book $book instance of Book
+     *
+     * @return void
+     */
+    public function __construct(Book $book)
+    {
+        $this->book = $book;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array
+     */
+    public function newBook()
+    {
+        $fields = [
+            'id',
+            'name',
+            'image',
+            'avg_rating'
+        ];
+        $books = $this->book->select($fields)
+                            ->orderBy('created_at', 'DESC')
+                            ->paginate(20);
+        return response()->json([
+            'books' => $books,
+            'success' => true
+        ], Response::HTTP_OK);
+    }
+}
