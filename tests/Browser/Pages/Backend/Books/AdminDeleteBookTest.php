51b7369aff47c20e68c5873082f0cd19af3b8a41
at-minhphan <minh.phan@asiantech.vn>
2018-02-06 16:10:12 +0700

    add test database

--- tests/Browser/Pages/Backend/Books/AdminDeleteBookTest.php
+++ tests/Browser/Pages/Backend/Books/AdminDeleteBookTest.php
@@ -10,6 +10,12 @@ use App\Model\Book;
 use App\Model\User;
 use App\Model\Category;
 use App\Model\Donator;
+use App\Model\Borrowing;
+use App\Model\QrCode;
+use App\Model\Rating;
+use App\Model\Post;
+use App\Model\Comment;
+use App\Model\Favorite;
 use Faker\Factory as Faker;
 
 class AdminDeleteBookTest extends BaseTestBook
@@ -26,10 +32,10 @@ class AdminDeleteBookTest extends BaseTestBook
         $userLogin = factory(User::class)->create(['role' => User::ROLE_USER]);
         $this->browse(function (Browser $browser) use ($userLogin) {
             $browser->loginAs($userLogin)
-                    ->resize(1200, 900)
-                    ->visit('/admin/users')
-                    ->assertPathIs('/login')
-                    ->assertSee('You are NOT an Administrator');
+            ->resize(1200, 900)
+            ->visit('/admin/users')
+            ->assertPathIs('/login')
+            ->assertSee('You are NOT an Administrator');
         });
     }
 
@@ -46,13 +52,13 @@ class AdminDeleteBookTest extends BaseTestBook
         $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
         $this->browse(function (Browser $browser) use ($userLogin, $bookID) {
             $browser->loginAs($userLogin)
-                    ->resize(1200, 900)
-                    ->visit('/admin/books')
-                    ->assertSee('LIST OF BOOK')
-                    ->assertVisible('#'.$bookID)
-                    ->assertVisible('.fa-trash-o')
-                    ->assertMissing('.bg-color-gray')
-                    ->assertMissing('.fa-history');
+            ->resize(1200, 900)
+            ->visit('/admin/books')
+            ->assertSee('LIST OF BOOK')
+            ->assertVisible('#'.$bookID)
+            ->assertVisible('.fa-trash-o')
+            ->assertMissing('.bg-color-gray')
+            ->assertMissing('.fa-history');
         });
     }
 
@@ -69,26 +75,13 @@ class AdminDeleteBookTest extends BaseTestBook
         $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
         $this->browse(function (Browser $browser) use ($userLogin, $book) {
             $browser->loginAs($userLogin)
-                    ->resize(1200, 900)
-                    ->visit('/admin/books')
-                    ->press('[book-id="'.$book->id.'"]')
-                    ->pause(1000)
-                    ->assertVisible('.bg-color-gray')
-                    ->assertVisible('.fa-history');
+            ->resize(1200, 900)
+            ->visit('/admin/books')
+            ->press('[book-id="'. $book->id. '"]')
+            ->pause(1000)
+            ->assertVisible('.bg-color-gray')
+            ->assertVisible('.fa-history');
         });
-        $this->assertSoftDeleted('books', [
-            'id' => $book->id,
-            'category_id' => $book->category_id,
-            'donator_id' => $book->donator_id,
-            'name' => $book->name,
-            'author' => $book->author,
-            'year' => $book->year,
-            'price' => $book->price,
-            'description' => $book->description,
-            'avg_rating' => $book->avg_rating,
-            'total_rating' => $book->total_rating,
-            'status' => $book->status,
-        ]);
     }
 
     /**
@@ -103,12 +96,12 @@ class AdminDeleteBookTest extends BaseTestBook
         $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
         $this->browse(function (Browser $browser) use ($userLogin, $bookID) {
             $browser->loginAs($userLogin)
-                    ->resize(1200, 900)
-                    ->visit('/admin/books')
-                    ->press('[book-id="'.$bookID.'"]')
-                    ->pause(1000)
-                    ->visit('/admin/books')
-                    ->assertMissing('#'.$bookID);
+            ->resize(1200, 900)
+            ->visit('/admin/books')
+            ->press('[book-id="'. $bookID. '"]')
+            ->pause(1000)
+            ->visit('/admin/books')
+            ->assertMissing('#'. $bookID);
         });
     }
 
@@ -120,35 +113,21 @@ class AdminDeleteBookTest extends BaseTestBook
     public function testClickRestore()
     {
         $this->makeListOfBook(1);
-        $book = DB::table('books')->first();
+        $bookID = DB::table('books')->pluck('id')->first();
         $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
-        $this->browse(function (Browser $browser) use ($userLogin, $book) {
+        $this->browse(function (Browser $browser) use ($userLogin, $bookID) {
             $browser->loginAs($userLogin)
-                    ->resize(1200, 900)
-                    ->visit('/admin/books')
-                    ->press('[book-id="'.$book->id.'"]')
-                    ->pause(1000)
-                    ->press('[book-id="'.$book->id.'"]')
-                    ->pause(1000)
-                    ->assertVisible('#'.$book->id)
-                    ->assertVisible('.fa-trash-o')
-                    ->assertMissing('.bg-color-gray')
-                    ->assertMissing('.fa-history');
+            ->resize(1200, 900)
+            ->visit('/admin/books')
+            ->press('[book-id="'. $bookID. '"]')
+            ->pause(1000)
+            ->press('[book-id="'. $bookID. '"]')
+            ->pause(1000)
+            ->assertVisible('#'. $bookID)
+            ->assertVisible('.fa-trash-o')
+            ->assertMissing('.bg-color-gray')
+            ->assertMissing('.fa-history');
         });
-        $this->assertDatabaseHas('books', [
-            'id' => $book->id,
-            'category_id' => $book->category_id,
-            'donator_id' => $book->donator_id,
-            'name' => $book->name,
-            'author' => $book->author,
-            'year' => $book->year,
-            'price' => $book->price,
-            'description' => $book->description,
-            'avg_rating' => $book->avg_rating,
-            'total_rating' => $book->total_rating,
-            'status' => $book->status,
-            'deleted_at' => null,
-        ]);
     }
 
     /**
@@ -163,14 +142,14 @@ class AdminDeleteBookTest extends BaseTestBook
         $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
         $this->browse(function (Browser $browser) use ($userLogin, $bookID) {
             $browser->loginAs($userLogin)
-                    ->resize(1200, 900)
-                    ->visit('/admin/books')
-                    ->press('[book-id="'.$bookID.'"]')
-                    ->pause(1000)
-                    ->press('[book-id="'.$bookID.'"]')
-                    ->pause(1000)
-                    ->visit('/admin/books')
-                    ->assertVisible('#'.$bookID);
+            ->resize(1200, 900)
+            ->visit('/admin/books')
+            ->press('[book-id="'. $bookID. '"]')
+            ->pause(1000)
+            ->press('[book-id="'. $bookID. '"]')
+            ->pause(1000)
+            ->visit('/admin/books')
+            ->assertVisible('#'. $bookID);
         });
     }
 
@@ -186,13 +165,122 @@ class AdminDeleteBookTest extends BaseTestBook
         $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
         $this->browse(function (Browser $browser) use ($userLogin, $bookID) {
             $browser->loginAs($userLogin)
-                    ->resize(1200, 900)
-                    ->visit('/admin/books')
-                    ->press('[book-id="'.$bookID.'"]')
-                    ->pause(1000)
-                    ->press('.btn-edit-'.$bookID)
-                    ->pause(1000)
-                    ->assertPathIs('/admin/books');
+            ->resize(1200, 900)
+            ->visit('/admin/books')
+            ->press('[book-id="'. $bookID. '"]')
+            ->pause(1000)
+            ->press('.btn-edit-'. $bookID)
+            ->pause(1000)
+            ->assertPathIs('/admin/books');
         });
+    }
+
+    /**
+     * When click delete button, book's relationship was soft deleted.
+     *
+     * @return void
+     */
+    public function testDatabaseWhenDeleted()
+    {
+        $this->makeABookAndItsRelationship();
+        $bookID = DB::table('books')->pluck('id')->first();
+        $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
+        $this->browse(function (Browser $browser) use ($userLogin, $bookID) {
+            $browser->loginAs($userLogin)
+                ->resize(1200, 900)
+                ->visit('/admin/books')
+                ->press('[book-id="'. $bookID. '"]')
+                ->pause(1000);
+        });
+        $this->assertSoftDeleted('books', ['id' => $bookID])
+            ->assertSoftDeleted('qrcodes', ['book_id' => $bookID])
+            ->assertSoftDeleted('borrowings', ['book_id' => $bookID])
+            ->assertSoftDeleted('ratings', ['book_id' => $bookID])
+            ->assertSoftDeleted('posts', ['book_id' => $bookID])
+            ->assertSoftDeleted('favorites', [
+                'favoritable_id' => $bookID,
+                'favoritable_type' => Favorite::TYPE_BOOK,
+            ]);
+        $posts = Post::withTrashed()->select('id')->where('book_id', $bookID);
+        foreach ($posts as $post) {
+            $this->assertSoftDeleted('favorites', [
+                'favoritable_id' => $post,
+                'favoritable_type' => Favorite::TYPE_POST,
+            ])
+                ->assertSoftDeleted('comments', ['post_id' => $post]);
+            $comments = Comment::withTrashed()->select('id')->where('post_id', $post);
+            foreach ($comments as $comment) {
+                $this->assertSoftDeleted('favorites', [
+                    'favoritable_id' => $comment,
+                    'favoritable_type' => Favorite::TYPE_COMMENT,
+                ]);
+            }
+        }
+    }
+
+    /**
+     * When click restore button, book's relationship was restored.
+     *
+     * @return void
+     */
+    public function testDatabaseWhenRestored()
+    {
+        $this->makeABookAndItsRelationship();
+        $bookID = DB::table('books')->pluck('id')->first();
+        $userLogin = factory(User::class)->create(['role' => User::ROLE_ADMIN]);
+        $this->browse(function (Browser $browser) use ($userLogin, $bookID) {
+            $browser->loginAs($userLogin)
+                ->resize(1200, 900)
+                ->visit('/admin/books')
+                ->press('[book-id="'. $bookID. '"]')
+                ->pause(1000)
+                ->press('[book-id="'. $bookID. '"]')
+                ->pause(1000);
+        });
+        $this->assertDatabaseHas('books', [
+                'id' => $bookID,
+                'deleted_at' => null,
+            ])
+            ->assertDatabaseHas('qrcodes', [
+                'book_id' => $bookID,
+                'deleted_at' => null,
+            ])
+            ->assertDatabaseHas('borrowings', [
+                'book_id' => $bookID,
+                'deleted_at' => null,
+            ])
+            ->assertDatabaseHas('ratings', [
+                'book_id' => $bookID,
+                'deleted_at' => null,
+            ])
+            ->assertDatabaseHas('posts', [
+                'book_id' => $bookID,
+                'deleted_at' => null,
+            ])
+            ->assertDatabaseHas('favorites', [
+                'favoritable_id' => $bookID,
+                'favoritable_type' => Favorite::TYPE_BOOK,
+                'deleted_at' => null,
+            ]);
+        $posts = Post::withTrashed()->select('id')->where('book_id', $bookID);
+        foreach ($posts as $post) {
+            $this->assertDatabaseHas('favorites', [
+                'favoritable_id' => $post,
+                'favoritable_type' => Favorite::TYPE_POST,
+                'deleted_at' => null,
+            ])
+                ->assertDatabaseHas('comments', [
+                    'post_id' => $post,
+                    'deleted_at' => null,
+                ]);
+            $comments = Comment::withTrashed()->select('id')->where('post_id', $post);
+            foreach ($comments as $comment) {
+                $this->assertDatabaseHas('favorites', [
+                    'favoritable_id' => $comment,
+                    'favoritable_type' => Favorite::TYPE_COMMENT,
+                    'deleted_at' => null,
+                ]);
+            }
+        }
     }
 }
