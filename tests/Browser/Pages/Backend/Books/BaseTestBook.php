1d5914788559b519a83ea230af987360c6df8b65
at-minhphan <minh.phan@asiantech.vn>
2018-02-09 01:06:33 +0700

    update BaseTestBook & AdminDeleteBookTest

--- tests/Browser/Pages/Backend/Books/BaseTestBook.php
+++ tests/Browser/Pages/Backend/Books/BaseTestBook.php
@@ -53,68 +53,70 @@ class BaseTestBook extends DuskTestCase
      *
      * @return Book.
      */
-    public function makeABookAndItsRelationship()
+    public function makeBookAndItsRelationship()
     {
-        //Make category, user, donator.
+        //Make 20 category, 20 user, 20 donator.
         $faker = Faker::create();
-        factory(Category::class)->create();
-        factory(User::class)->create();
-        $userID = DB::table('users')->pluck('id')->first();
-        factory(Donator::class)->create([
-            'user_id' => $userID,
+        factory(Category::class, 20)->create();
+        factory(User::class, 20)->create();
+        $userIds = DB::table('users')->pluck('id')->toArray();
+        factory(Donator::class, 20)->create([
+            'user_id' => $faker->unique()->randomElement($userIds),
         ]);
 
-        //Make book.
-        $categoryID = DB::table('categories')->pluck('id')->first();
-        $donatorID = DB::table('donators')->pluck('id')->first();
-        factory(Book::class)->create([
-            'category_id' => $categoryID,
-            'donator_id' => $donatorID,
+        //Make 10 book.
+        $categoryIds = DB::table('categories')->pluck('id')->toArray();
+        $donatorIds = DB::table('donators')->pluck('id')->toArray();
+        factory(Book::class, 10)->create([
+            'category_id' => $faker->randomElement($categoryIds),
+            'donator_id' => $faker->randomElement($donatorIds),
         ]);
 
-        //Make borrowing, qrcode, rating, post.
-        $bookID = DB::table('books')->pluck('id')->first();
-        factory(Borrowing::class)->create([
-            'user_id' => $userID,
-            'book_id' => $bookID,
+        //Make 20 borrowing, 20 qrcode, 20 rating, 20 post.
+        $bookIds = DB::table('books')->pluck('id')->toArray();
+        factory(Borrowing::class, 10)->create([
+            'user_id' => $faker->randomElement($userIds),
+            'book_id' => $faker->randomElement($bookIds),
         ]);
-        factory(QrCode::class)->create([
-            'book_id' => $bookID,
-            'prefix' => 'BAT-',
-            'code_id' => $faker->unique()->randomNumber(4),
-        ]);
-        factory(Rating::class)->create([
-            'book_id' => $bookID,
-            'user_id' => $userID,
+        for ($i = 0; $i < 20; $i++) {
+            factory(QrCode::class)->create([
+                'book_id' => $faker->randomElement($bookIds),
+                'code_id' => $faker->unique()->randomNumber(4),
+                'prefix' => 'BAT-'
+            ]);
+        }
+        factory(Rating::class, 20)->create([
+            'book_id' => $faker->randomElement($bookIds),
+            'user_id' => $faker->randomElement($userIds),
             'rating'  => rand(1,5),
         ]);
-        factory(Post::class)->create([
-            'book_id' => $bookID,
-            'user_id' => $userID,
+        factory(Post::class, 20)->create([
+            'book_id' => $faker->randomElement($bookIds),
+            'user_id' => $faker->randomElement($userIds),
         ]);
 
-        //Make comment.
-        $postID = DB::table('posts')->pluck('id')->first();
-        factory(Comment::class)->create([
-            'post_id' => $postID,
-            'user_id' => $userID,
+        //Make 20 comment.
+        $postIds = DB::table('posts')->pluck('id')->toArray();
+        factory(Comment::class, 20)->create([
+            'post_id' => $faker->randomElement($postIds),
+            'user_id' => $faker->randomElement($userIds),
         ]);
 
         //Make favorite for book, post, comment.
-        factory(Favorite::class)->create([
-            'user_id' => $userID,
-            'favoritable_id' => $bookID,
+        factory(Favorite::class, 20)->create([
+            'user_id' => $faker->randomElement($userIds),
+            'favoritable_id' => $faker->randomElement($bookIds),
             'favoritable_type' => Favorite::TYPE_BOOK,
         ]);
-        factory(Favorite::class)->create([
-            'user_id' => $userID,
-            'favoritable_id' => $postID,
+        factory(Favorite::class, 20)->create([
+            'user_id' => $faker->randomElement($userIds),
+            'favoritable_id' => $faker->randomElement($postIds),
             'favoritable_type' => Favorite::TYPE_POST,
         ]);
-        $commentID = DB::table('comments')->pluck('id')->first();
+        $commentIds = DB::table('comments')->pluck('id')->toArray();
         factory(Favorite::class)->create([
-            'user_id' => $userID,
-            'favoritable_id' => $commentID,
+            'user_id' => $faker->randomElement($userIds),
+            'favoritable_id' => $faker->randomElement($commentIds),
             'favoritable_type' => Favorite::TYPE_COMMENT,
         ]);
     }
