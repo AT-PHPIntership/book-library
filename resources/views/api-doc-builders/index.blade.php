e5d6ae822190d75bfd17987dd99878519b0bcbcd
at-minhphan <minh.phan@asiantech.vn>
2018-02-09 14:02:45 +0700

    add api categories

--- resources/views/api-doc-builders/index.blade.php
+++ resources/views/api-doc-builders/index.blade.php
@@ -2,3 +2,4 @@
 {!! file_get_contents(base_path('docs/api-references/sample.md')) !!}
 {!! file_get_contents(base_path('comments.md')) !!}
 {!! file_get_contents(base_path('posts.md')) !!}
+{!! file_get_contents(base_path('categories.md')) !!}
