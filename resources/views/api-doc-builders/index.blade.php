01e75cf0b51d6b62993d850b01d863ca8fc8a317
at-minhphan <minh.phan@asiantech.vn>
2018-02-09 18:35:03 +0700

    fix conflict

--- resources/views/api-doc-builders/index.blade.php
+++ resources/views/api-doc-builders/index.blade.php
@@ -1,2 +1,4 @@
 {!! file_get_contents(base_path('docs/api-references/api.md')) !!}
 {!! file_get_contents(base_path('docs/api-references/sample.md')) !!}
+{!! file_get_contents(base_path('docs/api-references/comments.md')) !!}
+{!! file_get_contents(base_path('docs/api-references/posts.md')) !!}
