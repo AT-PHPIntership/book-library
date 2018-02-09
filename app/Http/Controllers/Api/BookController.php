332acb23382b53b923788848307f748722a672b2
Nghia Vo V <nghia.vo@asiantech.vn>
2018-02-09 16:14:57 +0700

    implement api book detail

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+use App\Model\Book;
+
+class BookController extends Controller
+{
+    /**
+     * The Book implementation.
+     *
+     * @var Book
+     */
+    protected $book;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param Book $book instance of Book
+     *
+     * @return void
+     */
+    public function __construct(Book $book)
+    {
+        $this->book = $book;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id of book
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $fields = [
+            'name',
+            'author',
+            'year',
+            'page_number',
+            'price',
+            'image',
+            'description',
+            'avg_rating'
+        ];
+
+        $books = $this->book->select($fields)->findOrFail($id);
+        return response()->json([
+            'books' => $books,
+            'success' => true
+        ], Response::HTTP_OK);
+    }
+}
