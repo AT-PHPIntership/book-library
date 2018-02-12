5be09b93fd768bd83f98af929b7a5d371df38b75
at-minhphan <minh.phan@asiantech.vn>
2018-02-12 15:58:39 +0700

    add ApiController & CategoryController for api categories

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use App\Model\Category;
+
+class ApiController extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
