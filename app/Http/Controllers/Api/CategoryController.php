94f76a045a796eeb966134b3f0f8ae0d0546ba7c
at-minhphan <minh.phan@asiantech.vn>
2018-02-12 15:58:39 +0700

    add ApiController & CategoryController for api categories

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\ApiController;
+use App\Model\Category;
+use Illuminate\Pagination\Paginator;
+use App\Model\Book;
+use DB;
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Requests\CategoryUpdateRequest;
+use Illuminate\Http\Response;
+use App\Libraries\Traits\ApiTrait;
+
+class CategoryController extends ApiController
+{
+    use ApiTrait;
+
+    /**
+     * Response api list categories, its paginate and meta.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::select('id', 'name')
+            ->withCount('books')
+            ->groupBy('id')
+            ->paginate(config('define.page_length'))
+            ->toArray();
+        if (sizeof($categories['data']) === 0) {
+            return response()->json($this->fail(Response::HTTP_NOT_FOUND));
+        }
+        $api_categories = $this->convertPaginateApi($categories);
+        return response()->json($this->success($api_categories));
+    }
+}
