f6c1c1b477646aba80b739525042b4e198f565aa
at-phutran2 <phu.tran2@asiantech.vn>
2018-02-22 14:26:25 +0700

    implement top book borrow

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Libraries\Traits\ApiResponse;
+
+class ApiController extends Controller
+{
+    use ApiResponse;
+}
---
+++ app/Libraries/Traits/ApiResponse.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Libraries\Traits;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+trait ApiResponse
+{
+    /**
+     * Response list data
+     *
+     * @param Collection $collection collection
+     * @param int        $code       response status
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected function showAll(Collection $collection, $code = 200)
+    {
+        $collection = $this->paginate($collection);
+        return $this->structJson($collection, $code);
+    }
+
+    /**
+     * Pagination
+     *
+     * @param Collection $collection collection
+     *
+     * @return \Illuminate\Http\Response
+     */
+    protected function paginate(Collection $collection)
+    {
+        $rules = [
+            'pre_page' => 'integer|min:2|max:50'
+        ];
+        Validator::validate(request()->all(), $rules);
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $prePage = 20;
+        if (request()->has('pre_page')) {
+            $prePage = request()->pre_page;
+        }
+        $result = $collection->slice(($page - 1) * $prePage, $prePage);
+        $paginated = new LengthAwarePaginator($result, $collection->count(), $prePage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath()
+        ]);
+        $paginated->appends(request()->all());
+        return $paginated;
+    }
+
+    /**
+     * Structure of json
+     *
+     * @param LengthAwarePaginator $collection result response
+     * @param int                  $code       response status
+     *
+     * @return Illuminate\Support\Collection
+     */
+    public function structJson($collection, $code)
+    {
+        $collectionStruct = collect([
+            'meta' => [
+                'status' => 'successfully',
+                'code' => $code
+            ],
+            'data' => $collection->toArray()['data'],
+            'total' =>  $collection->total(),
+            'per_page' =>  $collection->perPage(),
+            'current_page' =>  $collection->currentPage(),
+            'total_pages' =>  $collection->lastPage(),
+            'links' => [
+                'prev' => $collection->previousPageUrl(),
+                'next' =>$collection->nextPageUrl(),
+            ]
+        ]);
+        return $collectionStruct;
+    }
+}
