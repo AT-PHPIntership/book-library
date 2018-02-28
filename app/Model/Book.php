a2458cb9e5483b0d1afc3bff003e4f2da1045698
at-anhngo <anh.ngo@asiantech.vn>
2018-02-28 11:25:44 +0700

    adding restore relationship with book if import already deleted book

--- app/Model/Book.php
+++ app/Model/Book.php
@@ -14,6 +14,8 @@ use Storage;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Iatstuti\Database\Support\CascadeSoftDeletes;
 use App\Libraries\Traits\SearchTrait;
+use App\Model\Comment;
+use App\Model\Favorite;
 
 class Book extends Model
 {
@@ -250,9 +252,46 @@ class Book extends Model
         $qrcode = QrCode::withTrashed()->where([['prefix', $qrCode['prefix']], ['code_id', $qrCode['code_id']]]);
         if ($qrcode->exists()) {
             $book = Book::withTrashed()->updateOrCreate(['id' => $qrcode->first()['book_id']], $data);
+            self::restore($qrcode->first()['book_id']);
             return $book;
         } else {
             return Book::create($data);
         }
+    }
+
+    /**
+     * Restore book and its relationship with id of book.
+     *
+     * @param int $id id of book
+     *
+     * @return boolean
+     */
+    public static function restore($id)
+    {
+        $deletedTime = Book::withTrashed()->where('id', $id)->pluck('deleted_at')->first();
+        //Get all posts ID was restored and its comments ID.
+        $postsID = Post::withTrashed()->where('book_id', $id)->where('deleted_at', $deletedTime)->pluck('id')->toArray();
+        $commentsID = Comment::withTrashed()->where('deleted_at', $deletedTime)->whereIn('post_id', $postsID)->pluck('id')->toArray();
+        //Restore book and its favorite.
+        $restored = Book::withTrashed()->whereNotNull('deleted_at')->where('id', $id)->restore();
+        if ($restored === 0) {
+            return false;
+        }
+        Favorite::withTrashed()->where('deleted_at', $deletedTime)->where('favoritable_type', Favorite::TYPE_BOOK)->where('favoritable_id', $id)->restore();
+
+        //Restore rating, qrcode, borrowing.
+        Rating::withTrashed()->where('book_id', $id)->where('deleted_at', $deletedTime)->restore();
+        QrCode::withTrashed()->where('book_id', $id)->where('deleted_at', $deletedTime)->restore();
+        Borrowing::withTrashed()->where('book_id', $id)->where('deleted_at', $deletedTime)->restore();
+
+        //Restore all post of this book and its favorite.
+        Post::withTrashed()->where('book_id', $id)->where('deleted_at', $deletedTime)->restore();
+        Favorite::withTrashed()->where('deleted_at', $deletedTime)->where('favoritable_type', Favorite::TYPE_POST)->whereIn('favoritable_id', $postsID)->restore();
+
+        //Restore all comments and its favorite.
+        Comment::withTrashed()->whereIn('post_id', $postsID)->where('deleted_at', $deletedTime)->restore();
+        Favorite::withTrashed()->where('deleted_at', $deletedTime)->where('favoritable_type', Favorite::TYPE_COMMENT)->whereIn('favoritable_id', $commentsID)->restore();
+
+        return true;
     }
 }
