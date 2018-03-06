d7fe55ea6d794cba10f3daac8b8e7d69eb6ca37f
Nghia Vo V <nghia.vo@asiantech.vn>
2018-03-06 15:24:51 +0700

    implement api update comment

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -2,13 +2,57 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Model\User;
 use App\Model\Comment;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-class CommentController extends Controller
+class CommentController extends ApiController
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @param Illuminate\Http\Request $request request
+     * @param App\Model\User          $user    instance of User
+     *
+     * @return void
+     */
+    public function __construct(Request $request, User $user)
+    {
+        parent::__construct($request, $user);
+    }
+
+    /**
+     * Update comment in Posts
+     *
+     * @param Request $request request
+     * @param int     $id      id of comment
+     *
+     * @return mixed
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $comment = Comment::findOrFail($id);
+            $userId = $this->user->id;
+            if ($comment->user_id != $userId) {
+                return response()->json(['message' => 'You dont have permission to edit'], 403);
+            }
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Comment not found'
+                ]
+            ], Response::HTTP_NOT_FOUND);
+        }
+        $comment->fill($request->all());
+        $comment->save();
+        return metaResponse($comment);
+    }
+
     /**
      * Delete comment by Post
      *
