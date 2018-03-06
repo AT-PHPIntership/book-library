3c7e9c08a9c99346ccdd93df869fd4bcc65d122c
at-minhphan <minh.phan@asiantech.vn>
2018-03-06 16:22:48 +0700

    fix unit test

--- config/define.php
+++ config/define.php
@@ -17,17 +17,19 @@
         'Korean' => 'Korean',
         'English' => 'English',
     ],
+    'book' => [
+      'item_limit' => 20
+    ],
     'messages' => [
       '404_not_found' => 'Page Not Found',
       '405_method_error' => 'Method Failure',
       '500_server_error' => 'Server Error',
       'token_not_found' => 'AccessToken Not Found',
-      '440_login_timeout' => 'Login Timeout'
-    ],
-    'book' => [
-      'item_limit' => 20,
-    ],
-    'qrcode' => [
+      '440_login_timeout' => 'Login Timeout',
+      'empty_comment' => 'Empty Comment',
+      'post_not_found' => 'Post Not Found',
+      'error_occurred' => 'Has Error Occurred, Please Try Again',
+      'parent_id_not_found' => 'Parent Id Not Found',
       'begin_prefix_pos' => 0,
       'end_prefix_pos' => 4,
     ]
