81a24e940df855449a3803fa488cc7911c0b5a69
at-minhphan <minh.phan@asiantech.vn>
2018-03-08 10:07:03 +0700

    fix circle CI

--- app/Http/Requests/Api/ApiNewCommentRequest.php
+++ app/Http/Requests/Api/ApiNewCommentRequest.php
@@ -20,6 +20,8 @@ class ApiNewCommentRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
+     * @param Request $request request
+     *
      * @return array
      */
     public function rules(Request $request)
